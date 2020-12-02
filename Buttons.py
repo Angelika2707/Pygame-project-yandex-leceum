31f909c1fd398a620f3c9e6d8edcb53c503e25ab
hexy00123 <r.izmajlov2018@yandex.ru>
2020-12-02 17:18:58 +0400

    Класс кнопки с примером работы

--- Buttons.py
+++ Buttons.py
@@ -2,78 +2,31 @@ import pygame
 
 
 class Button:
-    def __init__(self):
-        self.all_buttons = []
+    def __init__(self, surface, image_name, x, y, screen):
+        self.image = pygame.image.load(image_name)
+        self.x, self.y = x, y
+        self.width, self.height = self.image.get_width(), self.image.get_height()
+        self.screen = screen
+        self.draw_button(surface)
 
-    def create_button(self, surface, image_name, x, y, screen):
-        image = pygame.image.load(image_name)
-        info = {
-            'image_name': image_name,
-            'x': x,
-            'y': y,
-            'width': image.get_width(),
-            'height': image.get_height(),
-            'image': image,
-            'screen': screen
-        }
-        self.draw_button(surface, info)
-        self.all_buttons.append(info)
+    def draw_button(self, surface):
+        image_rect = self.image.get_rect(
+            bottomright=(self.x + self.width, self.y + self.height))
+        pygame.draw.rect(surface, pygame.Color(0, 0, 0),
+                         (self.x, self.y, self.width, self.height))
+        surface.blit(self.image, image_rect)
 
-    def draw_button(self, surface, info):
-        image_rect = info['image'].get_rect(
-            bottomright=(info['x'] + info['width'], info['y'] + info['height']))
-
-        surface.blit(info['image'], image_rect)
-        print('drawed')
-        return surface
-
-    def pressed(self, mouse, screen):
+    def pressed(self, mouse, screen=1):
         # прохожу по всем кнопкам в списке(далее попробую сделать множество)
         # проверяю кнопки по координате, и по расположению на экране
         # там где button_sound - должен быть звук
-        for button in self.all_buttons:
-            if mouse[0] >= button['x']:
-                if mouse[1] >= button['y']:
-                    if mouse[0] <= button['x'] + button['width']:
-                        if mouse[1] <= button['y'] + button['height']:
-                            if screen == button['screen']:
-                                print('button_sound', button['screen'])
-                                return True
-                            else:
-                                print('Кнопка нажалась, но кнопка находится на другом экране')
+        if mouse[0] >= self.x:
+            if mouse[1] >= self.y:
+                if mouse[0] <= self.x + self.width:
+                    if mouse[1] <= self.y + self.height:
+                        if screen == self.screen:
+                            print('button_sound', self.screen)
+                            return True
+                        else:
+                            print('Кнопка нажалась, но кнопка находится на другом экране')
         return False
-
-
-if __name__ == '__main__':
-    # Просто обычные настройки окна
-    pygame.init()
-    size = width, height = 500, 500
-    screen = pygame.display.set_mode(size)
-    fps = 50
-    clock = pygame.time.Clock()
-    running = True
-    # Создание менеджера кнопок
-    button_manager = Button()
-    button_settings_1 = {'surface': screen,
-                         'image_name': 'button.png',
-                         'x': 450,
-                         'y': 450,
-                         'screen': 1
-                         }
-
-    button_manager.create_button(**button_settings_1)
-
-    screens = 1
-
-    pygame.display.flip()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            # вызов метода кнопки pressed, см. его описание
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                button_manager.pressed(event.pos, screens)
-        pygame.display.flip()
-
-    pygame.quit()
