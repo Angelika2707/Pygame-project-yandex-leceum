23268cc5a151983b4a9cee502afa049131c899b4
Angelika2707 <angelika270702@gmail.com>
2021-01-05 11:09:09 +0400

    осталось сделать диалоги и паузу

--- main_game.py
+++ main_game.py
@@ -1,7 +1,7 @@
 import pygame
 from AllClasses import BaseLevelClass, AnimatedButton, Sprite, SwitchButton
 
-# button_right.png
+# turkey_image.png
 
 if __name__ == '__main__':
     pygame.init()
@@ -12,16 +12,23 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     spite1 = Sprite(['vol2.png'],
                     470, 550, 'rabbit_sound.mp3')
-    spite3 = Sprite(['mr_deer3.png'],
-                    650, 520, 'deer_roar.wav')
+    spite3 = Sprite(['m2.png'],
+                    650, 500, 'deer_roar.wav')
+    spite4 = Sprite(['mr_bird2.png'],
+                    1200, 450, 'crow.wav')
+    spite5 = Sprite(['turkey_image.png'],
+                    860, 430, 'turkey.wav')
+    spite6 = Sprite(['pig_image.png'],
+                    450, 570, 'pig.mp3')
     spite2 = Sprite(['mrs_pigion.png'],
                     1100, 620, 'pigion_sound.wav')
     button_right1 = AnimatedButton(['button_left.png'],
-                                   1600, 550, print,  'button_sound.ogg', True, 1)
+                                   1600, 550, print, 'button_sound.ogg', True, 1)
     button_right2 = AnimatedButton(['button_right.png'],
                                    300, 550, print, 'button_sound.ogg', True, 0)
     x = BaseLevelClass(['start_level_1.png', 'level_2.png'], ['piano_fon.mp3'],
-                       {'кнопки': [[spite1, 0], [spite2, 0], [spite3, 0], [button_right1, 0], [button_right2, 1]]},
+                       {'кнопки': [[spite1, 0], [spite2, 0], [spite3, 0], [button_right1, 0], [button_right2, 1],
+                                   [spite4, 1], [spite5, 1], [spite6, 1]]},
                        screen)
     x.draw_level()
     button_right1.function = x.next_screen
