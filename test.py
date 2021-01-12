dcfbe2fb5184e7d970a7ada791b4579a31bde627
DripCat4er <i_maklakov@bk.ru>
2021-01-12 17:52:45 +0400

    Git и желтые окружности

--- test.py
+++ test.py
@@ -1,43 +1,25 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
-from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
-from PyQt5.QtCore import Qt
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
-from random import randint
-
-
-class Test(QMainWindow):
+class Fockus(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
         uic.loadUi('1112.ui', self)
-
-        self.pushButton.setFixedSize(100, 100)
-        self.pushButton.clicked.connect(self.circle)
-
-        self.label = QLabel()
-        canvas = QPixmap(600, 600)
-        self.label.setPixmap(canvas)
-
-        layout = QGridLayout(self.centralwidget)
-        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
-        layout.addWidget(self.label, 1, 0)
-
-    def circle(self):
-        x, y = [randint(10, 500) for i in range(2)]
-        w, h = [randint(10, 100) for i in range(2)]
-        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
-        painter = QPainter(self.label.pixmap())
-        pen = QPen()
-        pen.setWidth(3)
-        pen.setColor(QColor(*['#FFD700']))
-        painter.setPen(pen)
-        painter.drawEllipse(x, y, w, h)
-        painter.end()
-        self.update()
+        self.pushButton.clicked.connect(self.new)
+    def initUI(self):
+        self.setGeometry(100, 100, 200, 200)
+        self.setWindowTitle('Проект по яндекс Лицею')
+    def new(self):
+        name = self.lineEdit.text()
+        self.lineEdit.setText(name)
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
-    import sys
     app = QApplication(sys.argv)
-    w = Test()
-    w.show()
-    sys.exit(app.exec_())
+    form = Fockus()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
