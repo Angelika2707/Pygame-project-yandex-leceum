09797c6eb6e614a5906ef542bff25bdbb311035c
DripCat4er <75274697+DripCat4er@users.noreply.github.com>
2021-01-15 20:27:08 +0400

    Add files via upload

--- scratch_3.py
+++ scratch_3.py
@@ -1,40 +1,96 @@
-import os
-import sys
-import pygame
+class Node:
 
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
-    MYEVENTTYPE = pygame.USEREVENT + 1
-    pygame.time.set_timer(MYEVENTTYPE, 10)
-    for event in pygame.event.get():
-        if event.type == MYEVENTTYPE:
-            print("Мое событие сработало")
+    def __init__(self, data, indexloc=None):
+        self.data = data
+        self.index = indexloc
+#ddd
+class Graph:
+
+    @classmethod
+    def create_from_nodes(self, nodes):
+        return Graph(len(nodes), len(nodes), nodes)
+
+    def __init__(self, row, col, nodes=None):
+        # установка матрица смежности
+        self.adj_mat = [[0] * col for _ in range(row)]
+        self.nodes = nodes
+        for i in range(len(self.nodes)):
+            self.nodes[i].index = i
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-        # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-image = load_image("dark-wood-1920x1080.jpg")
-image = pygame.transform.scale(image, (800, 400))
-pygame.display.flip()
-    # ожидание закрытия окна:
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-    # завершение работы:
+    def connect_dir(self, node1, node2, weight=1):
+        node1, node2 = self.get_index_from_node(node1), self.get_index_from_node(node2)
+        self.adj_mat[node1][node2] = weight
 
-pygame.quit()
+    # Опциональный весовой аргумент для поддержки алгоритма Дейкстры
+    def connect(self, node1, node2, weight=1):
+        self.connect_dir(node1, node2, weight)
+        self.connect_dir(node2, node1, weight)
+
+    # Получает ряд узла, отметить ненулевые объекты с их узлами в массиве self.nodes
+    # Выбирает любые ненулевые элементы, оставляя массив узлов
+    # которые являются connections_to (для ориентированного графа)
+    # Возвращает значение: массив кортежей (узел, вес)
+    def connections_from(self, node):
+        node = self.get_index_from_node(node)
+        return [(self.nodes[col_num], self.adj_mat[node][col_num]) for col_num in range(len(self.adj_mat[node])) if
+                self.adj_mat[node][col_num] != 0]
+
+    # Проводит матрицу к столбцу узлов
+    # Проводит любые ненулевые элементы узлу данного индекса ряда
+    # Выбирает только ненулевые элементы
+    # Обратите внимание, что для неориентированного графа
+    # используется connections_to ИЛИ connections_from
+    # Возвращает значение: массив кортежей (узел, вес)
+    def connections_to(self, node):
+        node = self.get_index_from_node(node)
+        column = [row[node] for row in self.adj_mat]
+        return [(self.nodes[row_num], column[row_num]) for row_num in range(len(column)) if column[row_num] != 0]
+
+    def print_adj_mat(self):
+        for row in self.adj_mat:
+            print(row)
+
+    def node(self, index):
+        return self.nodes[index]
+
+    def remove_conn(self, node1, node2):
+        self.remove_conn_dir(node1, node2)
+        self.remove_conn_dir(node2, node1)
+
+    # Убирает связь в направленной манере (nod1 к node2)
+    # Может принять номер индекса ИЛИ объект узла
+    def remove_conn_dir(self, node1, node2):
+        node1, node2 = self.get_index_from_node(node1), self.get_index_from_node(node2)
+        self.adj_mat[node1][node2] = 0
+
+        # Может пройти от node1 к node2
+
+    def can_traverse_dir(self, node1, node2):
+        node1, node2 = self.get_index_from_node(node1), self.get_index_from_node(node2)
+        return self.adj_mat[node1][node2] != 0
+
+    def has_conn(self, node1, node2):
+        return self.can_traverse_dir(node1, node2) or self.can_traverse_dir(node2, node1)
+
+    def add_node(self, node):
+        self.nodes.append(node)
+        node.index = len(self.nodes) - 1
+        for row in self.adj_mat:
+            row.append(0)
+        self.adj_mat.append([0] * (len(self.adj_mat) + 1))
+
+    # Получает вес, представленный перемещением от n1
+    # к n2. Принимает номера индексов ИЛИ объекты узлов
+    def get_weight(self, n1, n2):
+        node1, node2 = self.get_index_from_node(n1), self.get_index_from_node(n2)
+        return self.adj_mat[node1][node2]
+
+    # Разрешает проводить узлы ИЛИ индексы узлов  
+    def get_index_from_node(self, node):
+        if not isinstance(node, Node) and not isinstance(node, int):
+            raise ValueError("node must be an integer or a Node object")
+        if isinstance(node, int):
+            return node
+        else:
+            return node.index
