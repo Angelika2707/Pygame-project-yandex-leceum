e24963570ef090ca1d22b89ffb97634b0defeba1
Angelika2707 <angelika270702@gmail.com>
2021-01-25 21:59:53 +0400

    добавила сейф(незавершённый) и еще дилог

--- main_game.py
+++ main_game.py
@@ -49,7 +49,8 @@ if __name__ == '__main__':
                                    1480, 550, print, 'button_sound.ogg', True, 8)
     button_left_level = AnimatedButton(['button_right.png'],
                                         300, 550, print, 'button_sound.ogg', True, 7)
-    # page_room_tab
+    # door_exit_level
+    door_exit_level = AnimatedButton(['door_exit_level.png'], 750, 270, print, 'button_sound.ogg', True, 35)
     safe = AnimatedButton(['сейф2.png'], 360, 445, print, 'button_sound.ogg', True, 10)
     help_page = AnimatedButton(['подсказка_2.png'], 1270, 590, print, 'button_sound.ogg', True, 11)
     page_room_tab = AnimatedButton(['page_room_tab.png'],
@@ -58,8 +59,9 @@ if __name__ == '__main__':
                            700, 310, print, None, True, 6)
     door1 = AnimatedButton(['door_number2.png'],
                            1150, 310, print, None, True, 5)
+    delet = AnimatedButton(['cейф_пароль_0.png'], 700, 300, print, None)
     button_dialog_door = DialogSprite(['door_dialog.png'], 500, 100, print, door3_dil, 22)
-    #dialog_door_level = DialogSprite(['room_dialog.png'], 500, 100, print, door3_dil, 22)
+    dialog_door_level = DialogSprite(['room_dialog.png'], 400, 100, print, door_exit_level, 35)
     rabbit_dialog = DialogSprite(['image.png'], 500, 100, print, spite1, 8)
     deer_dialog = DialogSprite(['deer_dialog.png'], 500, 100, print, spite3, 9)
     pigion_dialog = DialogSprite(['pigion_dialog.png'], 500, 100, print, spite2, 10)
@@ -76,11 +78,13 @@ if __name__ == '__main__':
          [button_go_to_lift, 0], [button_dowm, 2], [buttons_in_lift, 2], [buttons_hall, 3], [door3, 4],
          [button_dowm2, 5], [door2, 4], [door1, 4], [button_dialog_door, -1], [door3_dil, 5], [button_dowm2, 6],
          [door_go_to_level, 6], [button_right_level, 7], [button_left_level, 8], [page_room_tab, 7], [button_dowm3, 9],
-         [safe, 7], [button_dowm4, 10], [help_page, 7], [button_dowm4, 11]],
+         [safe, 7], [button_dowm4, 10], [help_page, 7], [button_dowm4, 11], [door_exit_level, 8], [dialog_door_level, -1],
+         [delet, 10]],
         screen)
 
     x.draw_level()
     all_sprites2 = pygame.sprite.Group()
+    all_sprites2.add(dialog_door_level)
     all_sprites2.add(rabbit_dialog)
     all_sprites2.add(button_dialog_door)
     all_sprites2.add(deer_dialog)
@@ -88,6 +92,7 @@ if __name__ == '__main__':
     all_sprites2.add(bird_dialog)
     all_sprites2.add(turkey_dialog)
     all_sprites2.add(pig_dialog)
+    dialog_door_level.function = x.change_screen_off
     rabbit_dialog.function = x.change_screen_off
     button_dialog_door.function = x.change_screen_off
     deer_dialog.function = x.change_screen_off
@@ -120,6 +125,7 @@ if __name__ == '__main__':
     spite5.function = x.change_screen_on
     spite6.function = x.change_screen_on
     door3_dil.function = x.change_screen_on
+    door_exit_level.function = x.change_screen_on
     while running:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
